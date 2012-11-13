f6af359a83f742d9406a9117aac5c3c82049c149
Anton Zherdev <antonzherdev@MacBook-Air-Anton.local>
2012-11-13 20:49:34 +0400

    car1

--- TrainsOSX/CEOrtoSprite.m
+++ TrainsOSX/CEOrtoSprite.m
@@ -9,7 +9,6 @@
 - (void)setStart:(CGPoint)start end:(CGPoint)end {
     CGFloat angle = (end.x - start.x) / (end.y - start.y);
     [self setFlipX:angle > 0];
-
     self.position = ccp((end.x + start.x)/2 + _shift.x, (end.y + start.y)/2 + _shift.y);
 }
 @end
