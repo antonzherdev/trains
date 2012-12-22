a32a1ac6f3fbcb69d334ef0f9e4db81853678a32
antonzherdev <anton.zherdev@gmail.com>
2012-12-22 21:06:45 +0400

    switch state

--- TrainsOSX/CRSwitch.m
+++ TrainsOSX/CRSwitch.m
@@ -6,6 +6,8 @@
     CRRailForm _form2;
     CCSprite *_sprite;
     BOOL _composite;
+    CCSprite *_stateSprite;
+    int _state;
 }
 
 @synthesize form1 = _form1;
@@ -16,6 +18,7 @@
     if (self) {
         _form1 = form1;
         _form2 = form2;
+        _state = 0;
     }
 
     return self;
@@ -23,37 +26,75 @@
 
 - (void)onEnter {
     [super onEnter];
-    [_sprite removeFromParentAndCleanup:YES];
 
-    if(_composite) return;
+    if(!_composite) {
+        CGFloat x;
+        CGFloat y = 110;
+        BOOL flipX = NO;
+        if(_form1 == crRailFormX && _form2 == crRailFormTurn_X_Y) {
+            x = 0;
+        } else if(_form1 == crRailFormY && _form2 == crRailFormTurnXY) {
+            x = 0;
+            flipX = YES;
+        } else if(_form1 == crRailFormX && _form2 == crRailFormTurnXY) {
+            x = 220;
+        }else if(_form1 == crRailFormY && _form2 == crRailFormTurn_X_Y) {
+            x = 220;
+            flipX = YES;
+        } else if(_form1 == crRailFormX && _form2 == crRailFormTurn_XY) {
+            x = 440;
+        } else if(_form1 == crRailFormX && _form2 == crRailFormTurnX_Y) {
+            x = 660;
+        } else if(_form1 == crRailFormX && _form2 == crRailFormY) {
+            x = 880;
+        } else {
+            @throw @"Incorrect forms";
+        }
 
-    CGFloat x;
-    CGFloat y = 110;
-    BOOL flipX = NO;
-    if(_form1 == crRailFormX && _form2 == crRailFormTurn_X_Y) {
-        x = 0;
-    } else if(_form1 == crRailFormY && _form2 == crRailFormTurnXY) {
-        x = 0;
-        flipX = YES;
-    } else if(_form1 == crRailFormX && _form2 == crRailFormTurnXY) {
-        x = 220;
-    }else if(_form1 == crRailFormY && _form2 == crRailFormTurn_X_Y) {
-        x = 220;
-        flipX = YES;
-    } else if(_form1 == crRailFormX && _form2 == crRailFormTurn_XY) {
-        x = 440;
-    } else if(_form1 == crRailFormX && _form2 == crRailFormTurnX_Y) {
-        x = 660;
-    } else if(_form1 == crRailFormX && _form2 == crRailFormY) {
-        x = 880;
-    } else {
-        @throw @"Incorrect forms";
+        CGRect rect = CGRectMake(x, y, 220, 110);
+        _sprite = [CCSprite spriteWithFile:@"Rails.png" rect:rect];
+        if(flipX) [_sprite setFlipX:YES];
+        [self addChild:_sprite];
     }
 
-    CGRect rect = CGRectMake(x, y, 220, 110);
-    _sprite = [CCSprite spriteWithFile:@"Rails.png" rect:rect];
-    if(flipX) [_sprite setFlipX:YES];
-    [self addChild:_sprite];
+    [self updateStateSprite];
+}
+
+- (void)updateStateSprite {
+    if(_form1 == crRailFormX && _form2 == crRailFormY) return;
+
+    [_stateSprite removeFromParentAndCleanup:YES];
+    CRRailForm form = _state == 0 ? _form1 : _form2;
+    CRRailForm f2 = _state == 0 ? _form2 : _form1;
+
+    int y = 330;
+    int x = 0;
+    BOOL flipX = NO;
+    BOOL flipY = NO;
+    int px = 0;
+    int py = 0;
+    switch(form) {
+        case crRailFormNil:return;
+        case crRailFormX:
+            flipY = f2 == crRailFormTurnX_Y || f2 == crRailFormTurnXY;
+            flipX = flipY;
+            if(flipY) {px = 4; py = 1;}
+            break;
+        case crRailFormY:
+            flipY = f2 == crRailFormTurnX_Y || f2 == crRailFormTurn_X_Y;
+            flipX = !flipY;
+            if(flipY) {px = -4; py = 1;}
+            break;
+        case crRailFormTurnX_Y:return;
+        case crRailFormTurnXY:return;
+        case crRailFormTurn_XY:return;
+        case crRailFormTurn_X_Y:return;
+    }
+    _stateSprite = [CCSprite spriteWithFile:@"Rails.png" rect:CGRectMake(x, y, 220, 110)];
+    if(flipX) [_stateSprite setFlipX:YES];
+    if(flipY) [_stateSprite setFlipY:YES];
+    _stateSprite.position = ccp(px, py);
+    [self addChild:_stateSprite];
 }
 
 
@@ -67,6 +108,8 @@
     [super onExit];
     [_sprite removeFromParentAndCleanup:YES];
     _sprite = nil;
+    [_stateSprite removeFromParentAndCleanup:YES];
+    _stateSprite = nil;
 }
 
 
